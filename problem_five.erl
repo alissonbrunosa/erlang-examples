47a691db985e0ca40155be1bbf15ede1ef9560b0
Alisson Bruno <alissonbruno.sa@gmail.com>
2018-09-10 16:09:16 -0300

    Add problem #5 from dailycodingproblem.com

---
+++ problem_five.erl
@@ -0,0 +1,24 @@
+% This problem was asked by Jane Street.
+
+% cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+% For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+% Given this implementation of cons:
+%
+% def cons(a, b):
+%   def pair(f):
+%     return f(a, b)
+%   return pair
+% Implement car and cdr.
+
+-module(problem_five).
+-export([cons/2, car/1, cdr/1]).
+
+cons(A, B) ->
+  fun(Fn) -> Fn(A,B) end.
+
+car(Fn) ->
+  Fn(fun (A, _) -> A end).
+
+cdr(Fn) ->
+  Fn(fun(_, B) -> B end).
